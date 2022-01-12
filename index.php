6aa3fe3d8df82802c0ed062aeb219899f1c76c8b
Imam Shofiuddin <imamshofiuddin00@gmail.com>
2022-01-12 23:25:05 +0700

    memperbaiki tampilan

--- index.php
+++ index.php
@@ -1,22 +1,82 @@
-<!DOCTYPE html>
+<?php 
+
+include 'validation.php';
+
+?>
+
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reedem Voucher</title>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     <!-- CSS -->
     <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="center">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="center p-5 shadow bg-light">
         <h2>Masukkan Kode Voucher</h2>
-        <form action="validation.php" method="POST">
-            <label for="kode">Kode Voucher </label>
-            <input type="text" id="kode" name="kode" autofocus>
-            <button type="submit" name="submit">Pakai Kode</button>
-            <p>*Kode hanya bisa dipakai satu kali</p>
+        <form method="POST">
+          <div class="row g-3 align-items-center mt-3">
+            <div class="col-auto">
+              <label for="kode" class="col-form-label">Kode Voucher</label>
+            </div>
+            <div class="col-auto">
+              <input type="text" id="kode" class="form-control" name="kode" aria-describedby="passwordHelpInline" required>
+            </div>
+            <div class="col-auto">
+              <span id="passwordHelpInline" class="form-text">
+                Kode voucher hanya bisa dipakai sekali
+              </span>
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary">Pakai Kode</button>
+          </div>
         </form>
     </div>
-</body>
+
+    <!-- Alert Success -->
+    <?php if(isset($success)) : ?>
+      <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Selamat!</strong> Voucher berhasil digunakan.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    <?php endif ?>
+
+    <!-- Alert Danger -->
+    <?php if(isset($danger)) : ?>
+      <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Maaf</strong> Voucher gagal digunakan.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    <?php endif ?>
+
+    <!-- Alert Invalid -->
+    <?php if(isset($invalid)) : ?>
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Maaf</strong> Kode voucher yang anda masukkan salah.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    <?php endif ?>
+
+    <!-- Alert used voucher -->
+    <?php if(isset($used)) : ?>
+      <div class="alert alert-primary alert-dismissible fade show" role="alert">
+        <strong>Maaf</strong> Kode voucher sudah digunakan.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    <?php endif ?>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
 </html>
--- validation.php
+++ validation.php
@@ -7,29 +7,17 @@ if (isset($_POST["submit"])) {
     if($findID != 0) {
         $voucher = query("SELECT * FROM kode_voucher WHERE id = '$findID'");
         if( $voucher[0]['stats'] == "used") {
-            echo"<script>
-            alert('Kode Voucher Sudah Digunakan!');
-            document.location.href='index.php';
-            </script>";
+            $used = true;
         } else {
             if (ubah($voucher[0]) > 0) {
-                echo"<script>
-                alert('Kode Voucher Berhasil Digunakan!'); 
-                document.location.href='index.php';
-                </script>";
+                $success = true;
             } else {
-                echo"<script>
-                alert('Kode Voucher Gagal Digunakan!');
-                document.location.href='index.php';
-                </script>";             
+                $danger = true;           
             }
         }
         
     } else {
-            echo"<script>
-            alert('Kode Voucher Salah!');
-            document.location.href='index.php';
-            </script>";
+        $invalid = true;
     }
 }
 
